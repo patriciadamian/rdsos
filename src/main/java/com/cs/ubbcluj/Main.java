70dc3c3751f0490de82db756c9f198d2195b908a
achytimis <achytimis@gmail.com>
2019-01-13 19:39:37 +0200

    add packet sniffing template

--- src/main/java/com/cs/ubbcluj/Main.java
+++ src/main/java/com/cs/ubbcluj/Main.java
@@ -7,6 +7,18 @@ import java.io.InputStreamReader;
 import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.util.NifSelector;
 
+
+import java.io.IOException;
+import org.pcap4j.core.BpfProgram.BpfCompileMode;
+import org.pcap4j.core.NotOpenException;
+import org.pcap4j.core.PacketListener;
+import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
+import org.pcap4j.packet.Packet;
+import org.pcap4j.util.NifSelector;
+
 public class Main {
 
     public static void wifiAnalyser() throws IOException {
@@ -35,20 +47,40 @@ public class Main {
         System.out.println(stringBuilder.toString());
     }
 
-    public static void main(String[] args)  {
-        // The class that will store the network device
-        // we want to use for capturing.
-        PcapNetworkInterface device = null;
-
-        // Pcap4j comes with a convenient method for listing
-        // and choosing a network interface from the terminal
-        try {
-            // List the network devices available with a prompt
-            device = new NifSelector().selectNetworkInterface();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String [] args) throws PcapNativeException, IOException, NotOpenException, InterruptedException {
+        String filter = "tcp port 80";
+        if (args.length != 0) {
+            filter = args[0];
         }
 
-        System.out.println("You chose: " + device);
+        PcapNetworkInterface nif = new NifSelector().selectNetworkInterface();
+        if (nif == null) {
+            System.exit(1);
+        }
+
+        final PcapHandle handle = nif.openLive(65536, PromiscuousMode.PROMISCUOUS, 10);
+
+        if (filter != null && filter.length() != 0) {
+            handle.setFilter(filter, BpfCompileMode.OPTIMIZE);
+        }
+
+        PacketListener listener = new PacketListener() {
+            @Override
+            public void gotPacket(Packet packet) {
+                printPacket(packet, handle);
+            }
+        };
+
+        handle.loop(200, listener);
     }
+
+    private static void printPacket(Packet packet, PcapHandle ph) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("A packet captured at ")
+                .append(ph.getTimestamp())
+                .append(":");
+        System.out.println(sb);
+        System.out.println(packet);
+    }
+
 }
